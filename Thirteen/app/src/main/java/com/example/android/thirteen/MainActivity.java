09c05565a53083ae6e81df895573c9e6b6f02cd4
tcsauter <30811652+tcsauter@users.noreply.github.com>
2017-08-20 15:46:44 -0400

    Name Entry Screen
    Finishing details on Name Entry screen.

--- Thirteen/app/src/main/java/com/example/android/thirteen/MainActivity.java
+++ Thirteen/app/src/main/java/com/example/android/thirteen/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.name_entry);
+        setContentView(R.layout.activity_main);
     }
 }
