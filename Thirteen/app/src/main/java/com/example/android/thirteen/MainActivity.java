f0c7b57888c7aada6891070bcc325ce2f00f9f3f
tcsauter <30811652+tcsauter@users.noreply.github.com>
2017-08-20 18:34:22 -0400

    Misc changes
    Adds a version control file, and additions to strings and styles.

--- Thirteen/app/src/main/java/com/example/android/thirteen/MainActivity.java
+++ Thirteen/app/src/main/java/com/example/android/thirteen/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.thirteen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+    NumberPicker mNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
